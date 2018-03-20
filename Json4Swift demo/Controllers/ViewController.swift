151bedb8a3a5bf84b4b9e81a64d1c0c6aaf84ca8
cs.alhaider <cs.alhaider@gmail.com>
2018-03-20 14:39:17 +0300

    Adding Alamofire and SwiftyJson

--- Json4Swift demo/Controllers/ViewController.swift
+++ Json4Swift demo/Controllers/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import Alamofire
+import SVProgressHUD
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
     var jsonArray: [String]?
+    let jsonURL = "https://api.myjson.com/bins/17rcy5"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,37 +24,164 @@ class ViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
-        getJsonData()
+        //getJsonData()
+        getLongURLFromGoogle()
     }
 
     
     
     
     func getJsonData(){
-        
-        let jsonURL = "https://api.myjson.com/bins/17rcy5"
        
         guard let url = URL(string: jsonURL) else { return }
-        
             URLSession.shared.dataTask(with: url) { (data, response, err) in
                 guard let data = data else { return }
                 
                 do{
                     let expandedURL = try JSONDecoder().decode(AllURLData.self, from: data)
                     print(expandedURL)
-                    
                 }catch let jsonError {
-                    
                     print(jsonError)
-                    
                     let noConnection = UIAlertController(title: "Error! 😰", message: "Could not load data!", preferredStyle: .alert)
                     let dismiss = UIAlertAction(title: "OK", style: .default, handler: nil)
-                    
                     noConnection.addAction(dismiss)
                     self.present(noConnection, animated: true, completion: nil)
                 }
             }.resume()
         }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    var shortUrlClicksAllTimes: String?
+    var longUrlClicksAllTimes: String?
+    
+    var shortUrlClicksLastMonth: String?
+    var longUrlClicksLastMonth: String?
+    
+    var shortUrlClicksLastWeek: String?
+    var longUrlClicksLastWeek: String?
+    
+    var shortUrlClicksLastDay: String?
+    var longUrlClicksLastDay: String?
+    
+    var shortUrlClicksLastTwoHours: String?
+    var longUrlClicksLastTwoHours: String?
+    
+    
+    func getLongURLFromGoogle(){
+        
+        Alamofire.request(jsonURL, method: .get).responseJSON { (responds) in
+            
+//            if let JSON = responds.result.value {
+//                print(JSON)
+//            }
+            if responds.result.isSuccess {
+                //print(responds)
+                
+                let urlJSON : JSON = JSON(responds.result.value!)
+                
+                self.shortUrlClicksAllTimes = urlJSON["analytics"]["allTime"]["shortUrlClicks"].stringValue
+                self.longUrlClicksAllTimes = urlJSON["analytics"]["allTime"]["longUrlClicks"].stringValue
+                
+                self.shortUrlClicksLastMonth = urlJSON["analytics"]["month"]["shortUrlClicks"].stringValue
+                self.longUrlClicksLastMonth = urlJSON["analytics"]["month"]["longUrlClicks"].stringValue
+                
+                self.shortUrlClicksLastWeek = urlJSON["analytics"]["week"]["shortUrlClicks"].stringValue
+                self.longUrlClicksLastWeek = urlJSON["analytics"]["week"]["longUrlClicks"].stringValue
+                
+                self.shortUrlClicksLastDay = urlJSON["analytics"]["day"]["shortUrlClicks"].stringValue
+                self.longUrlClicksLastDay = urlJSON["analytics"]["day"]["longUrlClicks"].stringValue
+                
+                self.shortUrlClicksLastTwoHours = urlJSON["analytics"]["twoHours"]["shortUrlClicks"].stringValue
+                self.longUrlClicksLastTwoHours = urlJSON["analytics"]["twoHours"]["longUrlClicks"].stringValue
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                print(self.shortUrlClicksAllTimes!)
+                print(self.longUrlClicksAllTimes!)
+                
+                print(self.shortUrlClicksLastMonth!)
+                print(self.longUrlClicksLastMonth!)
+                
+                print(self.shortUrlClicksLastWeek!)
+                print(self.longUrlClicksLastWeek!)
+                
+                print(self.shortUrlClicksLastDay!)
+                print(self.longUrlClicksLastDay!)
+                
+                print(self.shortUrlClicksLastTwoHours!)
+                print(self.longUrlClicksLastTwoHours!)
+                
+            }
+        }
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'Json4Swift demo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Json4Swift demo
+
+pod 'SwiftyJSON'
+pod 'Alamofire'
+pod 'SVProgressHUD'
+pod 'ReachabilitySwift'
+
+end
